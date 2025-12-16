5d0543452fec0923db4544e88c865244c0a83b4d
cryptoshark12 <spotwork76@gmail.com>
2025-12-16 06:45:55 -0500

    Change RootLayout to an async function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
